16c4032d2056ec04b8b74e93aa01c70581ce76f9
montacir ait lahcen <montaciraitlahcen@gmail.com>
2025-12-17 03:25:01 +0100

    first page is done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        // Use the exact name provided by Google Fonts: 'Fraunces'
+        display: ["Fraunces", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
